8eb5fa9e6bd36a2286e2914625871d122e2a5ca3
Nazareno Armando <nazareno.rapisardi@mercadolibre.com>
2026-09-04 13:23:40 -0300

    Visit Japan Web completo: 5 de 5 QR de ingreso cargados

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service worker de la PWA del itinerario.
 // Estrategia: cache-first para el shell (funciona sin senial),
 // con actualizacion en segundo plano cuando hay conexion.
-const CACHE = 'kj26-d41d973b';
+const CACHE = 'kj26-a4db073b';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
