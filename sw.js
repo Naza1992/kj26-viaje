d337ff69aa331333c613f9fbcbceb6d002ac3258
Nazareno Armando <nazareno.rapisardi@mercadolibre.com>
2026-09-05 14:11:42 -0300

    Rediseno mobile: barra de 5 pestanias, hero solo en Hoy, chips y countdown compactos

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service worker de la PWA del itinerario.
 // Estrategia: cache-first para el shell (funciona sin senial),
 // con actualizacion en segundo plano cuando hay conexion.
-const CACHE = 'kj26-a4db073b';
+const CACHE = 'kj26-b275f168';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
