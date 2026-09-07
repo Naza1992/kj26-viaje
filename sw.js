a8a9baa0d7815fd960cd55a2e947e223ddec27fd
Nazareno Rapisardi <nazareno.rapisardi@mercadolibre.com>
2026-09-06 22:33:58 -0300

    Sacar pendiente obsoleto de QR Visit Japan Web (ya completo)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service worker de la PWA del itinerario.
 // Estrategia: cache-first para el shell (funciona sin senial),
 // con actualizacion en segundo plano cuando hay conexion.
-const CACHE = 'kj26-169657cf';
+const CACHE = 'kj26-c12e994a';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
