98b7d99ff476bf4127ebaaf364a4178ec4b84495
Nazareno Rapisardi <nazareno.rapisardi@mercadolibre.com>
2026-09-06 22:30:54 -0300

    Sumar seccion Plan recomendado: itinerario dia a dia

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service worker de la PWA del itinerario.
 // Estrategia: cache-first para el shell (funciona sin senial),
 // con actualizacion en segundo plano cuando hay conexion.
-const CACHE = 'kj26-4c944798';
+const CACHE = 'kj26-169657cf';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
